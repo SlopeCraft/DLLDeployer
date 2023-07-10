b8a4882123b8111028ff1aaeeeda11dfa5fe645a
虫 <tokinobug@163.com>
2023-07-11 06:11:25 +0800

    try to glob targets

--- DLLDeployer.cmake
+++ DLLDeployer.cmake
@@ -60,21 +60,21 @@ function(DLLD_get_location target result_var_name)
     set(${result_var_name} ${target} PARENT_SCOPE)
   endif ()
 endfunction()
-if(NOT DEFINED DLLD_msvc_utils)
-  if(${MSVC})
+if (NOT DEFINED DLLD_msvc_utils)
+  if (${MSVC})
     # if the compiler is msvc-like use msvc utils
     set(DLLD_msvc_utils_default_val ON)
-  else()
-    if(${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
+  else ()
+    if (${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
       # gcc
       set(DLLD_msvc_utils_default_val OFF)
     endif ()
 
-    if(${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
+    if (${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
       cmake_path(GET CMAKE_CXX_COMPILER PARENT_PATH compiler_prefix)
       cmake_path(GET compiler_prefix PARENT_PATH compiler_prefix)
 
-      if(EXISTS ${compiler_prefix}/bin/c++.exe)
+      if (EXISTS ${compiler_prefix}/bin/c++.exe)
         # Clang with mingw abi
         set(DLLD_msvc_utils_default_val OFF)
       else ()
@@ -84,7 +84,7 @@ if(NOT DEFINED DLLD_msvc_utils)
     endif ()
 
 
-    if(${CMAKE_CXX_COMPILER_ID} STREQUAL "MSVC")
+    if (${CMAKE_CXX_COMPILER_ID} STREQUAL "MSVC")
       set(DLLD_msvc_utils_default_val ON)
     endif ()
   endif ()
@@ -268,6 +268,7 @@ function(DLLD_library_type lib_file type_out_var_name)
 
 
   cmake_path(GET lib_file EXTENSION extension)
+  string(TOLOWER ${extension} extension)
   if (extension STREQUAL .a)
     set(${type_out_var_name} "static_lib" PARENT_SCOPE)
     return()
@@ -411,3 +412,292 @@ function(DLLD_deploy_lib target destination)
 
   message(FATAL_ERROR "Unhandled condition")
 endfunction(DLLD_deploy_lib)
+
+
+#function(DLLD_deploy_deps_for_target target destination)
+#
+#  set(target_dep_list)
+#  set(target_grab_property_list
+#    IMPORTED_LINK_DEPENDENT_LIBRARIES
+#    IMPORTED_LINK_DEPENDENT_LIBRARIES
+#    LINK_DEPENDS
+#    MANUALLY_ADDED_DEPENDENCIES
+#    IMPORTED_LIBNAME
+#    INTERFACE_LINK_LIBRARIES
+#    LINK_INTERFACE_LIBRARIES
+#    LINK_LIBRARIES)
+#  foreach (prop_name ${target_grab_property_list})
+#    unset(temp)
+#    get_target_property(temp ${target} ${prop_name})
+#    if (NOT temp)
+#      continue()
+#    endif ()
+#    message("${prop_name} of ${target} = ${temp}")
+#    list(APPEND target_dep_list ${temp})
+#  endforeach ()
+#
+#  message(STATUS "Going through the dependents of ${target}...")
+#  foreach (dep ${target_dep_list})
+#    # if dep is a generator expression, extract it
+#    string(FIND ${dep} "$<LINK_ONLY:" find_result)
+#    if (${find_result} GREATER_EQUAL 0)
+#      #message("Detected generator expression: ${dep}")
+#      string(LENGTH ${dep} dep_strlen)
+#      math(EXPR dep_strlen "${dep_strlen}-13")
+#      string(SUBSTRING ${dep} 12 ${dep_strlen} dep)
+#      #message("The extracted dep is: ${dep}")
+#    endif ()
+#
+#    if (EXISTS ${dep})
+#      DLLD_deploy_lib(${dep} ${destination})
+#      continue()
+#    endif ()
+#
+#    if (TARGET ${dep})
+#      DLLD_deploy_lib(${dep} ${destination})
+#      DLLD_deploy_deps_for_target(${dep} ${destination})
+#      continue()
+#    endif ()
+#
+#    find_library(dep_lib_location ${dep} QUIET)
+#    if (dep_lib_location)
+#      DLLD_deploy_lib(${dep_lib_location} ${destination})
+#      continue()
+#    endif ()
+#
+#    message(WARNING "No rule to handel dep named ${dep}")
+#  endforeach ()
+#
+#endfunction(DLLD_deploy_deps_for_target)
+
+#function(DLLD_glob_target_deps target out_var)
+#  unset(out_var PARENT_SCOPE)
+#  message(WARNING "This function will not work properly because fucking cmake doesn't support function recursing")
+#
+#  if(NOT TARGET ${target})
+#    message(FATAL_ERROR "${target} is not a target.")
+#  endif ()
+#
+#  set(prefix "${CMAKE_CURRENT_BINARY_DIR}/DLLD_glob_target_deps_")
+#  set(suffix "_deps.txt")
+#  file(WRITE "${prefix}${target}${suffix}" "")
+#
+#  cmake_parse_arguments(DLLD_glob_target_deps
+#    "NO_RECURSE;NO_SYSTEM_DLL" "" "" ${ARGN})
+#
+#  set(target_dep_list)
+#  set(target_grab_property_list
+#    IMPORTED_LINK_DEPENDENT_LIBRARIES
+#    IMPORTED_LINK_DEPENDENT_LIBRARIES
+#    LINK_DEPENDS
+#    MANUALLY_ADDED_DEPENDENCIES
+#    IMPORTED_LIBNAME
+#    INTERFACE_LINK_LIBRARIES
+#    LINK_INTERFACE_LIBRARIES
+#    LINK_LIBRARIES)
+#  foreach (prop_name ${target_grab_property_list})
+#    unset(temp)
+#    get_target_property(temp ${target} ${prop_name})
+#    if (NOT temp)
+#      continue()
+#    endif ()
+#    message("${prop_name} of ${target} = ${temp}")
+#    file(APPEND "${prefix}${target}${suffix}" ${temp})
+#    #list(APPEND target_dep_list ${temp})
+#  endforeach ()
+#
+#  message(STATUS "Going through the dependents of ${target}...")
+#  foreach (dep ${target_dep_list})
+#    # if dep is a generator expression, extract it
+#    string(FIND ${dep} "$<LINK_ONLY:" find_result)
+#    if (${find_result} GREATER_EQUAL 0)
+#      #message("Detected generator expression: ${dep}")
+#      string(LENGTH ${dep} dep_strlen)
+#      math(EXPR dep_strlen "${dep_strlen}-13")
+#      string(SUBSTRING ${dep} 12 ${dep_strlen} dep)
+#      #message("The extracted dep is: ${dep}")
+#    endif ()
+#
+#    # If is dep is a target
+#    if (TARGET ${dep})
+#      #set(${out_var} ${${out_var}} ${dep} PARENT_SCOPE)
+#      file(APPEND "${prefix}${target}${suffix}" ${dep})
+#      #list(APPEND result ${dep})
+#      message("Add target ${dep} to result")
+#      message("result = ${${out_var}}")
+#      unset(temp)
+#
+#      DLLD_glob_target_deps(${dep} dep_deps)
+#      message("dep_deps = ${dep_deps}")
+#      list(LENGTH dep_deps dep_deps_len)
+#      if(${dep_deps_len} GREATER 0)
+#        message("Add ${dep_deps} to result")
+#        #set(${out_var} ${${out_var}} ${dep_deps} PARENT_SCOPE)
+#        file(APPEND "${prefix}${target}${suffix}" ${dep_deps})
+#        #message("result = ${${out_var}}")
+#        #list(APPEND result x)
+#      endif ()
+#
+#      continue()
+#
+#    endif ()
+#
+#    if (EXISTS ${dep})
+#      file(APPEND "${prefix}${target}${suffix}" ${dep})
+#      #set(${out_var} ${${out_var}} ${dep} PARENT_SCOPE)
+#      message("Add file ${dep} to result")
+#      message("result = ${${out_var}}")
+#      continue()
+#    endif ()
+#
+#    find_library(dep_loc NAMES ${dep} QUIET)
+#    if (dep_loc)
+#      message("${dep} is recognized as ${dep_loc}")
+#      file(APPEND "${prefix}${target}${suffix}" ${dep_loc})
+#      #set(${out_var} ${${out_var}} ${dep_loc} PARENT_SCOPE)
+#      message("Add library ${dep_loc} to result")
+#      message("result = ${${out_var}}")
+#      continue()
+#    endif ()
+#
+#    message(WARNING "Unknown dep \"${dep}\" is not a file or target")
+#
+#  endforeach ()
+#  #file(APPEND "${prefix}${target}${suffix}" ${dep_deps})
+#  file(READ "${prefix}${target}${suffix}" temp_file_read_result)
+#  set(${out_var} ${temp_file_read_result} PARENT_SCOPE)
+#  message("end of function, result = ${out_var}")
+#
+#endfunction()
+
+
+#function(DLLD_deploy_for_exe target destination)
+#  cmake_parse_arguments(DLLD_deploy_for_exe
+#    "NO_RECURSE;INSTALL_SYSTEM_DLL" "" "" ${ARGN})
+#
+#  if (NOT TARGET ${target})
+#    message(FATAL_ERROR "The given target \"${target}\" is not a target.")
+#  endif ()
+#
+#  get_target_property(target_type ${target} TYPE)
+#
+##  if (NOT ${target_type} STREQUAL EXECUTABLE)
+##    message(FATAL_ERROR "The given target \"${target}\" is not an executable.")
+##  endif ()
+#
+#  message("Deploying dependents for \"${target}\"")
+#  set(target_grab_property_list
+#    IMPORTED_LINK_DEPENDENT_LIBRARIES
+#    IMPORTED_LINK_DEPENDENT_LIBRARIES
+#    LINK_DEPENDS
+#    MANUALLY_ADDED_DEPENDENCIES
+#    IMPORTED_LIBNAME
+#    INTERFACE_LINK_LIBRARIES
+#    LINK_INTERFACE_LIBRARIES
+#    LINK_LIBRARIES)
+#  foreach (prop_name ${target_grab_property_list})
+#    unset(temp)
+#    get_target_property(temp ${target} ${prop_name})
+#    if (NOT temp)
+#      continue()
+#    endif ()
+#    message("${prop_name} of ${target} = ${temp}")
+#    foreach (dep ${temp})
+#
+#      string(FIND ${dep} "$<LINK_ONLY:" find_result)
+#      if (${find_result} GREATER_EQUAL 0)
+#        message("Detected generator expression: ${dep}")
+#        string(LENGTH ${dep} dep_strlen)
+#        math(EXPR dep_strlen "${dep_strlen}-13")
+#        string(SUBSTRING ${dep} 12 ${dep_strlen} dep)
+#        #message("The extracted dep is: ${dep}")
+#      endif ()
+#
+#      if (EXISTS ${dep})
+#        message("Handing file ${dep}")
+#        DLLD_deploy_lib(${dep} ${destination})
+#        continue()
+#      endif ()
+#
+#      if (TARGET ${dep})
+#        message("Handing target ${dep}")
+#        DLLD_deploy_lib(${dep} ${destination})
+#        DLLD_deploy_for_exe(${dep} ${destination})
+#        continue()
+#      endif ()
+#
+#      find_library(dep_lib_location ${dep} QUIET)
+#      if (dep_lib_location)
+#        message("Handing name only library ${dep}")
+#        DLLD_deploy_lib(${dep_lib_location} ${destination})
+#        continue()
+#      endif ()
+#
+#      message(WARNING "No rule to handel dep named ${dep}")
+#    endforeach ()
+#
+#  endforeach ()
+#endfunction()
+
+function(test_recurse folder out_var)
+    unset(${out_var} PARENT_SCOPE)
+
+    file(GLOB temp_list "${folder}/*")
+    set(file_list_temp)
+    foreach (item ${temp_list})
+        if(IS_DIRECTORY ${item})
+            test_recurse(${item} temp_var)
+            list(APPEND file_list_temp ${temp_var})
+        else ()
+            list(APPEND file_list_temp ${item})
+        endif ()
+    endforeach ()
+    set(${out_var} ${file_list_temp} PARENT_SCOPE)
+endfunction(test_recurse)
+
+function(DLLD_get_target_deps target_name out_var)
+    unset(${out_var} PARENT_SCOPE)
+    if(NOT TARGET ${target_name})
+        return()
+    endif ()
+
+      set(target_grab_property_list
+        IMPORTED_LINK_DEPENDENT_LIBRARIES
+        IMPORTED_LINK_DEPENDENT_LIBRARIES
+        LINK_DEPENDS
+        MANUALLY_ADDED_DEPENDENCIES
+        IMPORTED_LIBNAME
+        INTERFACE_LINK_LIBRARIES
+        LINK_INTERFACE_LIBRARIES
+        LINK_LIBRARIES)
+
+    set(temp_list)
+    foreach (prop_name ${target_grab_property_list})
+        get_target_property(prop_values ${target_name} ${prop_name})
+        if(prop_values)
+            list(APPEND temp_list ${prop_values})
+        endif ()
+    endforeach ()
+
+    set(${out_var} ${temp_list} PARENT_SCOPE)
+endfunction()
+
+
+function(glob_deps_recurse target_name out_var)
+    unset(${out_var} PARENT_SCOPE)
+    if(NOT TARGET ${target_name})
+        return()
+    endif ()
+
+    DLLD_get_target_deps(${target_name} target_deps)
+    set(dep_list_recurse)
+    foreach (dep ${target_deps})
+        list(APPEND dep_list_recurse ${dep})
+        if(TARGET ${dep})
+            glob_deps_recurse(${dep} deps_of_dep)
+            list(APPEND dep_list_recurse ${deps_of_dep})
+        endif ()
+    endforeach ()
+
+    set(${out_var} ${dep_list_recurse} PARENT_SCOPE)
+endfunction()
