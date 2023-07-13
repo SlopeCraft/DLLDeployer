f6712dbf2b12fb4e9c613b35de93ccc98de85e67
虫 <tokinobug@163.com>
2023-07-13 21:55:16 +0800

    fix windows build without qt

--- main.cpp
+++ main.cpp
@@ -1,21 +1,29 @@
+#include <iostream>
 #include <omp.h>
 #include <zip.h>
-#include <iostream>
+#ifdef QT_SUPPORT
 #include <QCoreApplication>
 #include <QMainWindow>
+#endif
 
-int main(int argc,char**argv) {
+int main(int argc, char **argv) {
 
-  QCoreApplication qapp{argc,argv};
+#ifdef QT_SUPPORT
+  QCoreApplication qapp{argc, argv};
 
   QMainWindow wind;
   wind.show();
+#endif
 
   omp_set_num_threads(20);
 
   zip_close(nullptr);
 
-  std::cout<<"DLLDeployer!"<<std::endl;
+  std::cout << "DLLDeployer!" << std::endl;
 
+#ifdef QT_SUPPORT
   return qapp.exec();
+#else
+  return 0;
+#endif
 }
